2325f0f3fda7f03486ca5028adfa068dd3a4df1e
Fradven <frederic.vanovermeire@gmail.com>
2022-01-12 00:12:52 +0100

    logo

--- src/component/Pokedex.tsx
+++ src/component/Pokedex.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Fetch from './Fetch'
 
 export default function Pokedex() {
+    /* const logo = new URL("../img/pokedex.png", import.meta.url); */
     return (
         <>
+            {/* <img src="{logo}" alt="" /> */}
             <h1>Pokédex Online</h1>
             <Fetch />
         </>
