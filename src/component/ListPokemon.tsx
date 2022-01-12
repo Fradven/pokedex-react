987e24b5a4f831bed30f85e519585d02625b36cd
Fradven <frederic.vanovermeire@gmail.com>
2022-01-12 16:14:59 +0100

    display sprites & id of pokemon

--- src/component/ListPokemon.tsx
+++ src/component/ListPokemon.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { axios } from './axios.js'
 import './ListPokemon.scss'
 
 interface Props {
@@ -6,11 +7,33 @@ interface Props {
 }
 
 const ListPokemon: React.FC<Props> = ({name}) => {
+    const [pokemon, setPokemon] = useState<any>("")
+
+    const getPokemon = async() => {
+        const res = await axios.get(`${name}`)
+
+        setPokemon(res.data);
+    }
+    
+    useEffect(() => {
+        getPokemon()
+    }, [])
+    console.log(pokemon)
+    
     return (
+        <>
+        {pokemon ? 
         <div className='pokemon__card'>
+
+            <p className="pokemon__height">#{pokemon.id}</p>
+            <div className="pokemon__sprite">
+                <img src={pokemon.sprites.front_default} />
+            </div>
             <h3 className="pokemon__name">{name}</h3>
             <button className="pokemon__detail">More Details</button>
-        </div>
+        </div> 
+        : <div>Loading</div> } 
+            </>
     )
 }
 
