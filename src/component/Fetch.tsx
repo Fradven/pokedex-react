e51d2d26a74e8b3393fe8d85d87e2a9c1a08935e
Fradven <frederic.vanovermeire@gmail.com>
2022-01-19 14:30:19 +0100

    clean up the code

--- src/component/Fetch.tsx
+++ src/component/Fetch.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react'
+import React, {useState } from 'react'
 import  axios  from 'axios'
 import InfiniteScroll from './infiniteScroll/InfiniteScroll';
 import ListPokemon from './ListPokemon';
@@ -6,10 +6,9 @@ import './ListPokemon.scss'
 
 
 export default function Fetch() {
-    const [events, setEvents] = useState<any >([]);
+    const [events, setEvents] = useState<any>([]);
     const [currentPage, setCurrentPage] = useState("https://pokeapi.co/api/v2/pokemon")
     const [nextPage, setNextPage] = useState("")
-    const [waitNext, setWaitNext] = useState(false)
     const [loading, setLoading] = useState(false)
 
 
@@ -18,55 +17,18 @@ export default function Fetch() {
         
         const response = await axios.get(currentPage)
         const data = response.data;
-        /* const getNext = (await axios.get(currentPage)).data.next */
         const next = data.next;
         setLoading(true)
         setNextPage(await next)
         
         if (response && data.results) setEvents((prev: any)=>[...prev, ...data.results]);
-        setWaitNext(false)
         setLoading(false)
-    }
-
-    const checkNext = async() => {
-        const getNext = (await axios.get(currentPage)).data.next
-
-        if (getNext !== nextPage) {
-            console.log("uwu")
-            setWaitNext(false)
-        } else {
-            console.log("owo")
-            setWaitNext(true)
-        }
-
-    }
-
-    useEffect (() => {
-        console.log("next page in useEffect:" + nextPage)
-        checkNext()
-        if (waitNext === true) {
-            setWaitNext(false)
-            console.log("currentPage in useEffect: " + currentPage)
-        }
-    }, [nextPage])
-
-    //function to input the "nextPage" into the "currentPage"
-    const gotoNextPage = async() => { 
-        setWaitNext(false)
-
-        setCurrentPage(nextPage)
-        console.log("currentPage in gotoNext : " + currentPage)   
     }
     
     const hitBottom = async() => {
         const getNext = (await axios.get(currentPage)).data.next
         if (getNext !== nextPage) {
-            console.log("do it")
             setCurrentPage(getNext)
-            getEvents()
-        } else {
-            console.log("do not")
-            gotoNextPage()
             getEvents()
         }
     }
@@ -74,9 +36,6 @@ export default function Fetch() {
     const hasMoreData = nextPage !== null ? true : false
     return (
         <>
-        <div className='page-btn'>
-                {/*  nextPage === null ? null : <button onClick={gotoNextPage}>Next</button>  */}            
-            </div>
             <InfiniteScroll
                 onBottomHit={hitBottom}
                 hasMoreData={hasMoreData}
