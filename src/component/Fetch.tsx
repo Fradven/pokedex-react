69050d7d6180e1d1c78c3a541d6576bda4af95e3
Fradven <frederic.vanovermeire@gmail.com>
2022-01-19 15:00:35 +0100

    add comment to explaine code

--- src/component/Fetch.tsx
+++ src/component/Fetch.tsx
@@ -12,25 +12,25 @@ export default function Fetch() {
     const [loading, setLoading] = useState(false)
 
 
-    //fetch the data from the current page of the api
+    //fetch the data from the current page of the api and set the nextPage
     const getEvents = async() => {
         
-        const response = await axios.get(currentPage)
+        const response = await axios.get(currentPage) 
         const data = response.data;
         const next = data.next;
         setLoading(true)
-        setNextPage(await next)
+        setNextPage(await next) //add the value of the next page of the api to nextPage
         
-        if (response && data.results) setEvents((prev: any)=>[...prev, ...data.results]);
+        if (response && data.results) setEvents((prev: any)=>[...prev, ...data.results]); //check for response before adding results of the api to setEvents
         setLoading(false)
     }
     
+    //set the current page to the new page and use then call getevents to fetch add more data
     const hitBottom = async() => {
-        const getNext = (await axios.get(currentPage)).data.next
-        if (getNext !== nextPage) {
-            setCurrentPage(getNext)
-            getEvents()
-        }
+        const getNext = (await axios.get(currentPage)).data.next //get the link for the next page
+            setCurrentPage(getNext) //change the current page link into the next page from getNext
+            getEvents() //fetch more data and add them to events
+        
     }
 
     const hasMoreData = nextPage !== null ? true : false
--- src/component/infiniteScroll/InfiniteScroll.tsx
+++ src/component/infiniteScroll/InfiniteScroll.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useEffect, useState } from 'react'
 
 type Props = {
-  onBottomHit: () => void;
-  isLoading: boolean;
-  hasMoreData: boolean;
-  loadOnMount: boolean;
+  onBottomHit: () => void; //function that will activate at end of the use ref <div>
+  isLoading: boolean; //check for loading
+  hasMoreData: boolean; //check for more data
+  loadOnMount: boolean; //true to load data on page load
 };
 
 
@@ -16,16 +16,20 @@ const InfiniteScroll: React.FC<Props> = ({
   children,
 }) => {
   const [initialLoad, setInitialLoad] = useState(true);
-  const contentRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null); //if the element of the ref to be > then null
 
+  //return a boolean depending if ref is null or not
   function isBottom(ref: React.RefObject<HTMLDivElement>) {
+    //if ref is null return false
     if (!ref.current) {
       return false;
     }
+    //if ref is at the bottom return true
     return ref.current.getBoundingClientRect().bottom <= window.innerHeight;
   }
   
 
+  //on intial laod, return to top of page and call function linked to onBottomHit() to load the data for the first time
   useEffect(() => {
     if (loadOnMount && initialLoad) {
       window.scrollTo(0, 0)
@@ -34,14 +38,16 @@ const InfiniteScroll: React.FC<Props> = ({
     }
   }, [onBottomHit, loadOnMount, initialLoad]);
 
+  //add an event listener for scrolling, call onBottomHit() once the user reach the bottom and removre the event listener when done
   useEffect(() => {
+    //check if not loading, has more data and has reach bottom => activate onBottomHit()
     const onScroll = () => {
       if (!isLoading && hasMoreData && isBottom(contentRef)) {
         onBottomHit();
       }
     };
-    document.addEventListener('scroll', onScroll);
-    return () => document.removeEventListener('scroll', onScroll);
+    document.addEventListener('scroll', onScroll); //listen to scroll so that onScroll can get data for isBottom()
+    return () => document.removeEventListener('scroll', onScroll); //remove listener (don't know why yet)
   }, [onBottomHit, isLoading, hasMoreData]);
 
   return <div className='pokemon' ref={contentRef}>{children}</div>;
