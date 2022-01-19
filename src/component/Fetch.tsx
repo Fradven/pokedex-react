27539763e347a060233868c31a2ae593443661c3
Fradven <frederic.vanovermeire@gmail.com>
2022-01-19 10:44:22 +0100

    clean the code a bit to test

--- src/component/Fetch.tsx
+++ src/component/Fetch.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect } from 'react'
 import  axios  from 'axios'
 import InfiniteScroll from './infiniteScroll/InfiniteScroll';
 import ListPokemon from './ListPokemon';
@@ -8,69 +8,85 @@ import './ListPokemon.scss'
 export default function Fetch() {
     const [events, setEvents] = useState<any >([]);
     const [currentPage, setCurrentPage] = useState("https://pokeapi.co/api/v2/pokemon")
-    const [nextPage, setNextPage] = useState<any>("")
-    const [previousPage, setPreviousPage] = useState("")
-    //const [loading, setLoading] = useState(false)
+    const [nextPage, setNextPage] = useState("")
+    const [waitNext, setWaitNext] = useState(false)
 
     //fetch the data from the current page of the api
     const getEvents = async() => {
         
         const response = await axios.get(currentPage)
         const data = response.data;
+        const getNext = (await axios.get(currentPage)).data.next
+        console.log("nextPage :" + nextPage)
+        console.log("data.next: " + getNext)
+        console.log("current: " + currentPage)
         
-
-        //if (response && (await response).data.results) setEvents((await response).data.results);  //verify the response and input the data into "events"
         if (response && data.results) setEvents((prev: any)=>[...prev, ...data.results]);
+        setWaitNext(false)
+        
         //setLoading(false)
     }
     
     useEffect(() => {
+        window.scrollTo(0, 0)
         getEvents()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    useEffect (() => {
+        console.log("this next:" + nextPage)
+        setWaitNext(!waitNext)
+        if (waitNext === true) {
+            
+            setWaitNext(true)
+            setCurrentPage(nextPage)
+            console.log("current effect: " + currentPage)
+        }
+    }, [nextPage])
+
     //function to input the "nextPage" into the "currentPage"
-    const gotoNextPage = async() => {
+    const gotoNextPage = async() => { 
+        setWaitNext(false)
         const response = await axios.get(currentPage)
-        setNextPage(response.data.next);
-        console.log("nextPage : " + nextPage)
-        
+        const data = response.data;
+        const next = data.next;
+        setNextPage(await next)
+            
         setCurrentPage(nextPage)
-        console.log("currentPage : " + currentPage)
+        console.log("currentPage : " + currentPage)   
     }
     //function to input the "PreviousPage" into "currentPage"
-    const gotoPreviousPage = async() => {
+    /* const gotoPreviousPage = async() => {
         const response = await axios.get(currentPage)
         const data = response.data;
         const previous = data.previous;
         setPreviousPage(previous)
 
         setCurrentPage(previousPage)
-    }
+    } */
+
+    
 
     const hitBottom = () => {
-        console.log('hit bottom')
         gotoNextPage();
         getEvents();
     }
-    const hasMoreData = nextPage === null ? true : false
+
+    //const hasMoreData = nextPage === null ? true : false
     return (
         <>
+        <div className='page-btn'>
+                { nextPage === null ? null : <button onClick={gotoNextPage}>Next</button> }            
+            </div>
             <InfiniteScroll
-                hasMoreData={hasMoreData}
-                //isLoading={loading}
                 onBottomHit={hitBottom}
-                loadOnMount={true}
     >
                 {/* If "events" is not empty, use ".map" to go through every result and display the, using "ListPokemon" */}
                 {(events.length === 0) ? 'loading' : events.map((event: { name: string; })=>
                 <ListPokemon name={event.name} key={event.name} />)}
             </InfiniteScroll>
 
-            <div className='page-btn'>
-                { previousPage === null ? null : <button onClick={gotoPreviousPage}>Previous</button> }
-                { nextPage === null ? null : <button onClick={gotoNextPage}>Next</button> }            
-            </div>
+            
         </>
     )
 }
--- src/component/infiniteScroll/InfiniteScroll.tsx
+++ src/component/infiniteScroll/InfiniteScroll.tsx
@@ -1,69 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 
 type Props = {
     onBottomHit: () => void; //the function where we fetch the information 
-    //isLoading: boolean; //set to see if is loiding or not
-    hasMoreData: boolean; //check if there's more data
-    loadOnMount: boolean; //load initial page
   };
-
-  /**
-   * Verify the postion of the user compared to ref div, if below return true
-   * @param ref 
-   * @returns boolean
-   */
- /*  function isBottom(ref: React.RefObject<HTMLDivElement>) {
-    if (!ref.current) {
-        console.log('coucou')
-      return false;
-    }
-    console.log('cou')
-    return ref.current.getBoundingClientRect().bottom <= window.innerHeight;
-  } */
 
 const InfiniteScroll: React.FC<Props> = ({
     onBottomHit,
-    //isLoading,
-    //hasMoreData,
-    children,
-    loadOnMount
+    children
 }) =>{
-    const [initialLoad, setInitialLoad] = useState(true);
     const contentRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (loadOnMount && initialLoad) {
-      //onBottomHit();
-      setInitialLoad(false);
-    }
-  }, [onBottomHit, loadOnMount, initialLoad]);
-
-  /* useEffect(() => {
-    const onScroll = () => {
-      if (!isLoading && hasMoreData && isBottom(contentRef)) {
-        onBottomHit();
-        console.log('bump')
-      }
-    };
-    document.addEventListener('scroll', onScroll);
-    return () => document.removeEventListener('scroll', onScroll);
-  }, []); */
   const handleScroll = () => {
     if (!contentRef.current) {
       return false;
     }
-    //@typescript-eslint/no-unused-expressions
-    if (contentRef.current.getBoundingClientRect().bottom <= window.innerHeight) onBottomHit()
+    if (contentRef.current.getBoundingClientRect().bottom <= window.innerHeight) {
+      onBottomHit()
+    }
   }
+
   useEffect(() => {
+    
     window.addEventListener('scroll', handleScroll, {
       passive: true
     });
-
+    
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return <div className='pokemon' ref={contentRef}>{children}</div>;
 }
 
