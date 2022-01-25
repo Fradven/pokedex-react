efc907f4f97d3f5d9c33c6e6036be2a59c180ce1
Fradven <frederic.vanovermeire@gmail.com>
2022-01-25 16:35:41 +0100

    Sidebar initial setup

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function Nav() {
+    const [active, setActive] = useState(false)
+
+    const showSidebar = () => setActive(!active)
     return (
         <>
-    
+            <div className="nav-button">
+                <Link to="#" className='menu-bar' onClick={showSidebar}>
+
+                </Link>
+            </div>
+            <nav className={active ? 'sidebar' : 'sidebar active'}>
+                <ul className="sidebar__link-container">
+                    <li className="sidebar__link">
+                        
+                    </li>
+                    <li className="sidebar__link">
+                        
+                    </li>
+                    <li className="sidebar__link">
+                        
+                    </li>
+                </ul>
+            </nav>
         </>
         );
 }
