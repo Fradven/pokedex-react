bef81391b416e48080aa8500744ccbb9b8442e13
Fradven <frederic.vanovermeire@gmail.com>
2022-01-26 11:47:10 +0100

    created a js folder & sidebarData file

--- src/component/ListPokemon.tsx
+++ src/component/ListPokemon.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { axios } from './axios.js'
+import { axios } from './javascript/axios.js'
 import loading from '../img/loading.gif'
 import './ListPokemon.scss'
 
---
+++ src/component/javascript/sidebarData.js
@@ -0,0 +1,15 @@
+export const SidebarData = [
+    {   
+        title: 'Home',
+        path: '/'
+    },
+    {   
+        title: 'Pokedex',
+        path: '/pokedex'
+    },
+    {   
+        title: 'Movedex',
+        path: '/movedex'
+    }
+    
+]
