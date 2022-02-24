173cb646584ec905a53a7ae4813848ab79012c58
Fradven <frederic.vanovermeire@gmail.com>
2022-02-24 10:23:40 +0100

    setup chart list

---
+++ src/javascript/typeChart.js
@@ -0,0 +1,74 @@
+export const typeChart = [
+    {
+        type: 'normal',
+        value: 1
+    },
+    {
+        type: 'fire',
+        value: 1
+    },
+    {
+        type: 'water',
+        value: 1
+    },
+    {
+        type: 'grass',
+        value: 1
+    },
+    {
+        type: 'electric',
+        value: 1
+    },
+    {
+        type: 'ice',
+        value: 1
+    },
+    {
+        type: 'fighting',
+        value: 1
+    },
+    {
+        type: 'poison',
+        value: 1
+    },
+    {
+        type: 'ground',
+        value: 1
+    },
+    {
+        type: 'flying',
+        value: 1
+    },
+    {
+        type: 'psychic',
+        value: 1
+    },
+    {
+        type: 'bug',
+        value: 1
+    },
+    {
+        type: 'rock',
+        value: 1
+    },
+    {
+        type: 'ghost',
+        value: 1
+    },
+    {
+        type: 'dragon',
+        value: 1
+    },
+    {
+        type: 'dark',
+        value: 1
+    },
+    {
+        type: 'steel',
+        value: 1
+    },
+    {
+        type: 'fairy',
+        value: 1
+    },
+]
